2b67326dfbd55b2bf3e7f095f19c67ec838ad04a
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-04-04 18:56:59 +0200

    Fix iframe mistakes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// Object Constructors
 function Phone(brand, price, color) {
     this.brand = brand;
     this.price = price;
     this.color = color;
 }
 
+// Prototypes
 Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 };
