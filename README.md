bd60bbdf67891e68e089ce133eae5b3a783c3f7c
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-04-04 18:30:59 +0200

    Create new OOP project

---
+++ README.md
@@ -0,0 +1,10 @@
+11.4 - OOP classes
+
+https://codepen.io/sophie_gosia/
+
+How to run this project: 
+
+    $ npm run init-project 
+    $ npm run build
+    $ npm run watch
+    $ npm run test 
---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+};
+
+// Objects
+var samsungGalaxyS6 = new Phone("Samsung", 1800, "black");
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var onePlusOne = new Phone("One Plus", 1600, "silver");
+
+iPhone6S.printInfo();
+onePlusOne.printInfo();
+samsungGalaxyS6.printInfo();
